a5aae6a248ba0cca0712dbce9375c496fdd000ca
ALEXAIORUSHUN <fsccasebox@gmail.com>
2024-11-29 21:31:46 +0100

    HSS Text Above Right
    Jobbfelül kiír egy szöveget, amit megadsz a "watermark.lua-ban".

---
+++ HSSText/__resource.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+
+author 'ALEXAIORUS'
+description 'Loadingscreen'
+version '1.0.0'
+
+client_scripts {
+	'watermark.lua'
+}
---
+++ HSSText/watermark.lua
@@ -0,0 +1,54 @@
+-- Jobbfelső sarokba config rész
+
+servername = "Hungarian Scripts Shop"
+
+offset = {x = 0.890, y = 0.00}
+
+
+rgb = {r = 32, g = 32, b = 32}
+
+
+alpha = 255
+
+
+scale = 0.5
+
+
+font = 1
+
+
+bringontherainbows = true
+
+-- Szívárvány színátmenet 
+
+Citizen.CreateThread(function()
+	while true do
+		Wait(1)
+
+		if bringontherainbows then
+			rgb = RGBRainbow(1)
+		end
+		SetTextColour(rgb.r, rgb.g, rgb.b, alpha)
+
+		SetTextFont(font)
+		SetTextScale(scale, scale)
+		SetTextWrap(0.0, 1.0)
+		SetTextCentre(false)
+		SetTextDropshadow(2, 2, 0, 0, 0)
+		SetTextEdge(1, 0, 0, 0, 205)
+		SetTextEntry("STRING")
+		AddTextComponentString(servername)
+		DrawText(offset.x, offset.y)
+	end
+end)
+
+function RGBRainbow(frequency)
+    local result = {}
+    local curtime = GetGameTimer() / 1000
+
+    result.r = math.floor(math.sin(curtime * frequency + 0) * 127 + 128)
+    result.g = math.floor(math.sin(curtime * frequency + 2) * 127 + 128)
+    result.b = math.floor(math.sin(curtime * frequency + 4) * 127 + 128)
+
+    return result
+end
